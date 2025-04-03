80bd679b1a41eab21dcbdd981988fd523423915b
haringmgapirata <rexbellilumantao13@gmail.com>
2025-04-03 14:43:11 +0800

    deleted a /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/final-reactjs-int-django/',
+  base: '/final-reactjs-int-django',
 })
